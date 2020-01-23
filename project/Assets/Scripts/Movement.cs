0bf0ce740c6d9885f611c1d9b684bf8ac2248364
GrandNewStart <hbjw123@naver.com>
2020-01-23 18:15:41 +0900

    Project initiated. Currently working on player movements and animations.

---
+++ project/Assets/Scripts/Movement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+    private Vector2 movement;
+    private Rigidbody2D rb;
+    private Animator animator;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
+         movement.y = Input.GetAxisRaw("Vertical") * speed * Time.deltaTime;
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement);
+    }
+}
