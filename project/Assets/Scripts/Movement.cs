f8db371f556f1cf35effc7a7fc830203b07f7b46
GrandNewStart <hbjw123@naver.com>
2020-01-24 00:56:21 +0900

    Completed movement animations.

--- project/Assets/Scripts/Movement.cs
+++ project/Assets/Scripts/Movement.cs
@@ -5,28 +5,67 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public float speed;
+    private int lastDirection;
     private Vector2 movement;
     private Rigidbody2D rb;
     private Animator animator;
+    private string[] idleDirections = { "Idle_N", "Idle_NW", "Idle_W", "Idle_SW", "Idle_S", "Idle_SE", "Idle_E", "Idle_NE" };
+    private string[] walkDirections = { "Walk_N", "Walk_NW", "Walk_W", "Walk_SW", "Walk_S", "Walk_SE", "Walk_E", "Walk_NE" };
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        movement = Vector2.down;
+        SetDirection(movement);
     }
 
     private void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
-         movement.y = Input.GetAxisRaw("Vertical") * speed * Time.deltaTime;
-
-        animator.SetFloat("Horizontal", movement.x);
-        animator.SetFloat("Vertical", movement.y);
-        animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
     private void FixedUpdate()
     {
+        Move();
+    }
+
+    void Move()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
+        movement.y = Input.GetAxisRaw("Vertical") * speed * Time.deltaTime;
+        SetDirection(movement);
         rb.MovePosition(rb.position + movement);
+    }
+
+    void SetDirection(Vector2 direction)
+    {
+        string[] directionArray = null;
+
+        if (direction.magnitude < 0.001f)
+        {
+            directionArray = idleDirections;
+        }
+        else
+        {
+            directionArray = walkDirections;
+            lastDirection = DirectionToIndex(direction);
+        }
+
+        animator.Play(directionArray[lastDirection]);
+    }
+
+    int DirectionToIndex(Vector2 direction)
+    {
+        Vector2 normalizedDir = direction.normalized;
+        float step = 360 / 8;
+        float offset = step / 2;
+        float angle = Vector2.SignedAngle(Vector2.up, normalizedDir);
+        angle += offset;
+        if (angle < 0f)
+        {
+            angle += 360;
+        }
+        float stepCount = angle / step;
+        return Mathf.FloorToInt(stepCount);
     }
 }
